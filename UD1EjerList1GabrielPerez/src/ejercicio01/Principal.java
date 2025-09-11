3cf0d64a880563b6518dc032db23ef03495f302a
GabrielPerezMarquez <perez.magab24@triana.salesianos.edu>
2025-09-11 02:08:41 +0200

    ejericio1List1

---
+++ UD1EjerList1GabrielPerez/src/ejercicio01/Principal.java
@@ -0,0 +1,49 @@
+package ejercicio01;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int codigo=1234;
+		int respuesta;
+		String respStr;
+		int cero=0;
+		int numIntentosDisp=4;
+		boolean acierto = false;
+		
+		while(numIntentosDisp > cero && !acierto ) {
+			System.out.println("Introduce el código (4 digitos)");
+			System.out.println("Tienes");
+			respStr = sc.nextLine();
+			respuesta = Integer.parseInt(respStr);
+			
+			if (respStr.length() != 4 ) {
+				System.out.println("Debe ser un código de 4 digitos");
+			} else {
+				if (respuesta == codigo) {
+					System.out.println("Código correcto. Puerta abierta!");
+					acierto = true;
+				} else {
+					System.out.println("Código incorrecto");
+					numIntentosDisp--;					
+				}
+			
+			}
+					
+			
+		}
+		
+		if(numIntentosDisp == cero) {
+			System.out.println("Has alcanzado el número de intentos. Acceso bloqueado");
+		}
+		
+		
+		sc.close();
+		
+	}
+}
