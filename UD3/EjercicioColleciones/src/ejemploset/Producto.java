6f74ddf51afc39f409074f138aa749974e175c6f
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-11-27 12:45:39 +0100

    Ejercicio de colecciones empezado

---
+++ UD3/EjercicioColleciones/src/ejemploset/Producto.java
@@ -0,0 +1,22 @@
+package ejemploset;
+
+public class Producto {
+	
+	
+	private Long id;
+	private String nombre;
+	private double precio;
+	private int stock;
+	private String categoria;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
